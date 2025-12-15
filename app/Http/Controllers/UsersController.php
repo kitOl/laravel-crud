aa1fa4cb57c1debd0a3be272e1eea4ae25f3bcea
Oleg Kitaev <kitaev_oleg@mail.ru>
2025-12-15 21:40:56 +0300

    card for user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -49,7 +49,7 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        return view('show');
+        return view('show', compact('user'));
     }
 
     /**
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,6 +1,18 @@
 @extends('layout')
-@section('title', 'Users')
+@section('title', 'User ' . $user->name)
 
 @section('content')
-    show
+    <a type="button" class="btn btn-secondary" href="{{ route('users.index') }}">Back to list</a>
+
+    <div class="card mt-3" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{ $user->id }}</li>
+            <li class="list-group-item">Name: {{ $user->name }}</li>
+            <li class="list-group-item">Email: {{ $user->email }}</li>
+            <li class="list-group-item">Created at: {{ $user->created_at->format('d/m/y H:i:s') }}</li>
+            <li class="list-group-item">Updated at: {{ $user->updated_at->format('d/m/y H:i:s') }}</li>
+        </ul>
+    </div>
+
+    <a type="button" class="btn btn-warning mt-3" href="{{ route('users.edit', $user) }}">Edit</a>
 @endsection
